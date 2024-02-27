448fe28fc94843d5c30cc30de2e58341b63fd508
sheryopticweb <sheharyarseo@gmail.com>
2024-02-27 19:49:40 +0500

    path change

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Laralink">
     <!-- Favicon Icon -->
-    <link rel="icon" href="{{asset('frontend/img/favicon.png')}}">
+    <link rel="icon" href="{{URL::asset('frontend/img/favicon.png')}}">
     <!-- Site Title -->
     <title>Opti-web-serve</title>
     <link rel="stylesheet" href="{{URL::asset('frontend/css/plugins/bootstrap.min.css')}}">
